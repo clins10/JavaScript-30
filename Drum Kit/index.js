e5bee4e1fbf9105866a839b88fb50eaab229fac8
Viashima <72166830+clins10@users.noreply.github.com>
2022-06-28 20:32:35 +0100

    drum kit

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,16 +1,17 @@
-
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; // skip it if it's not a 
+  e.target.classList.remove('playing');
+}
+ 
 function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.Code}"]`);
-  const key = document.querySelector(`.key[data-key="${e.Code}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.div[data-key="${e.key}"]`);
   if (!audio) return; // stop function from running all together
+  keyy.classList.add('playing');
   audio.currentTime = 0; //rewind to the start
   audio.play();
-  key.classList.add('playing');
 }
-function removeTransition(e) {
-  if (e.propertyName !== 'transform') return; // skip it if it's not a transform
-  this.classList.remove('playing');
-}
-const keys = document.querySelectorAll('.key');
+
+const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
