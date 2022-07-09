36b5118c3d3069fdffb4ed5c0f8d756c79b19a8a
Viashima <72166830+clins10@users.noreply.github.com>
2022-07-09 13:57:00 +0100

    day-7

--- TypeAhead/script.js
+++ TypeAhead/script.js
@@ -1,4 +1,5 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+//  const endpoint = 'http://maps.googleapis.com/maps/api/js?sensor=false&libraries=places&key=AIzaSyCXFJ-lc7cHHcEklG2_oIhTnPKTWsLwHEU';
 
 const cities = [];
 fetch(endpoint)
